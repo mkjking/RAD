24b1f281d3cf7a63df3bdea6095c9aed388728a1
Blayde <lorderite@gmail.com>
2019-11-12 08:48:31 +0800

    Index

---
+++ Index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+    <!-- 
+        This php file is the welcome page of the website.
+        This file is a page on its own and should be referenced as such.
+        This welcome page includes top 10 results
+
+        Author: Blayde Dietsch
+        Date: 12/11/2019
+    -->
+
+    <head>
+        <title>Movie Rentals</title>
+        <link rel="stylesheet" type="text/css" href="Rentalcss.css"/>
+    </head>
+
+    <body>
+        <header>
+            <?php
+                require_once'functions/nav.php'; 
+            ?>
+        </header>
+
+        <div class="container">
+            <div class="content">
+                <h1>Welcome To The Home Menu</h1>
+
+                <p>This is the home menu. now yes,
+                    this is a beautiful website.
+                    But it is so much more.
+                    Here, you can do many things.
+                    Just click on the tabs and find out!
+                </p>
+
+                <?php 
+                    require'functions/Connection.php';
+                ?>
+            </div>
+        </div>
+        
+        <footer>
+            <?php
+                require_once'functions/footer.php'; 
+            ?>      
+        </footer>
+    </body>
+</html>
