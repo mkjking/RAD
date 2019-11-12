c2d6e84db59d650a4912d338bd749b086eb10a9c
Blayde <lorderite@gmail.com>
2019-11-12 08:31:34 +0800

    Work

---
+++ nav.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+    <!-- 
+        This php file is to handle expressing navigation.
+        To safely represent the content of this file please contain it in a HEADER
+
+        Author: Blayde Dietsch
+        Date: 12/11/2019
+    -->
+
+    <div class="headerContainer">
+        
+        <div class="title">
+            <h1>Movie Rentals</h1>
+        </div>
+                
+        <nav>
+            <ul>
+                <li><a href="Index.php">Home</a></li>                 
+                <li><a href="Import.php">Import</a></li>
+                <li><a href="MovieList.php">Movie List</a></li>
+                <li><a href="Search.php">Search</a></li>
+                <li><a href="Top10.php">Top 10</a></li>                
+            </ul>
+        </nav>
+    </div>
+</html>
