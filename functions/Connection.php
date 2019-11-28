49ec45701bd64d41c455be5a0a27eb7731fc082b
mkjking <m128155@tafe.wa.edu.au>
2019-11-28 14:45:12 +0800

    Apply PEAR code standard to Connection.php

--- functions/Connection.php
+++ functions/Connection.php
@@ -1,34 +1,49 @@
 <!DOCTYPE html>
 
-	<!-- 
+    <!-- 
         This php file is to handle a CONNECTION to the SQL Database.
         To safely represent the content of this file please contain it in a DIV
 
         Author: Blayde Dietsch, Mitchel King, Noah Jackson
         Date: 12/11/2019
-    -->
+       -->
 
-	<?php
-		
-		//Variables for connection
-		include 'config.php';
+    <?php
+    /**
+    PHP version 7
 
-		$conn = mysqli_connect($host,$user,$password);
-		//Report on connection Status
-		if (!$conn) {
-			echo "<p style=\"font-size: auto\">Connection Status: <span style=\"color: Red\"> Offline</span></p>";
-			
-		}else {
-			echo "<p style=\"font-size: auto\">Connection Status: <span style=\"color: Lime\"> Online</span></p>";
+    @category SQL
 
-			$databaseconn = mysqli_query($conn, "USE $database");
+    @package RAD
 
-			//Report on connection Status
-			if (!$databaseconn) {
-				echo "<p style=\"font-size: auto\">Database Status: <span style=\"color: Red\"> Offline</span></p>";
-			}else{
-				echo "<p style=\"font-size: auto\">Database Status: <span style=\"color: Lime\"> Online</span></p>";
-			}
-		}
-	?>
+    @author Original Author <mitchel_king@icloud.com>
+
+    @license http://www.php.net/license PHP license 7
+
+    @link http:/pear.php.net
+     **/
+    //Variables for connection
+    require 'config.php';
+
+    $conn = mysqli_connect($host, $user, $password);
+    //Report on connection Status
+    if (!$conn) {
+            echo "<p style=\"font-size: auto\">
+                  Connection Status: <span style=\"color: Red\"> Offline</span></p>";
+    } else {
+            echo "<p style=\"font-size: auto\">
+            Connection Status: <span style=\"color: Lime\"> Online</span></p>";
+
+            $databaseconn = mysqli_query($conn, "USE $database");
+
+            //Report on connection Status
+        if (!$databaseconn) {
+                echo "<p style=\"font-size: auto\">
+                Database Status: <span style=\"color: Red\"> Offline</span></p>";
+        } else {
+                echo "<p style=\"font-size: auto\">
+                Database Status: <span style=\"color: Lime\"> Online</span></p>";
+        }
+    }
+    ?>
 </html>
