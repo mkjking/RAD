21efce9b7e0ec3759e133dbab9f3bd607c7f2f59
mkjking <m128155@tafe.wa.edu.au>
2019-11-28 12:59:57 +0800

    Apply PEAR code standard to Admin.php

--- Admin.php
+++ Admin.php
@@ -3,7 +3,8 @@
     <!-- 
         This php file is the ADMIN page of the website.
         This file is a page on its own and should be referenced as such.
-        This admin page allows admins to remove people from the newsletter subscription
+        This admin page allows admins to remove people from the 
+        newsletter subscription
 
         Author: Blayde Dietsch, Mitchel King, Noah Jackson
         Date: 17/11/2019
@@ -18,7 +19,27 @@
     <body>
         <header>
             <?php
-                require_once'functions/nav.php'; 
+            /**
+            PHP version 7
+
+            @category SQL
+
+            @package RAD
+
+            @author Original Author <mitchel_king@icloud.com>
+
+            @license http://www.php.net/license PHP license 7
+
+            @link http:/pear.php.net
+             **/
+            /** 
+            Nav file
+            
+            @file nav.php
+
+            renders the navigation bar
+             **/
+            require_once'functions/nav.php';
             ?>
         </header>
 
@@ -32,49 +53,65 @@
             </div>
             <div class="content">
                 <?php 
-                    require('functions/AdminLogin.php');
+                require'functions/AdminLogin.php';
                     //CHECK FOR LOGIN
-                    if($loginStatus) {
+                if ($loginStatus) {
                         //USER TABLE
                         echo "<h1>SUBSCRIBED USERS</h1>";
-                        require 'functions/Usertable.php';
+                        incude 'functions/Usertable.php';
                         echo "<h1>RATINGS HISTORY</h1>";
                     
                         //Historical ratings
                         include 'config.php';
-                        $conn = mysqli_connect($host,$user,$password, $database);
-                        $resultSet = $conn->query("SELECT ts FROM ratings_tbl ORDER BY ts DESC");                       
+                        $conn = mysqli_connect($host, $user, $password, $database);
+                        $resultSet = $conn->query(
+                            "SELECT ts FROM ratings_tbl
+                         ORDER BY ts DESC"
+                        );
                         
                         echo'<form action="ratingsHistory.php" method="post">
                             <select name="ratings">';
-                                while($rows = $resultSet->fetch_assoc()){
-                                $ratingDate = $rows["ts"];
-                                echo "<option value='$ratingDate'>$ratingDate</option>";
-                                }
+                    while ($rows = $resultSet->fetch_assoc()) {
+                            $ratingDate = $rows["ts"];
+                        echo "<option value='$ratingDate'>$ratingDate</option>";
+                    }
                         echo'    </select>
-                            <p><input type="submit" name="btnGetGraph" value="Retreive Data"/></p>
+                            <p><input type="submit" name="btnGetGraph"
+                            value="Retreive Data"/></p>
                         </form>';
                 
                         //PASSWORD AND REMOVE FORMS
                         echo' <h1>CHANGE PASSWORD</h1>
-                              <form action="AdminControl.php" method="post">
-                                  <label>Enter Your Email: </label><input type="text" name="adminEmail"><br>
-                                  <label>Change Your Password: </label><input type="text" name="adminPassword"><br>
-                                  <input type="submit" name="btnChange" value="Change"/>
+                              <form action="AdminControl.php"
+                               method="post">
+                                  <label>Enter Your Email: </label>
+                                  <input type="text" name="adminEmail"><br>
+                                  <label>Change Your Password: </label>
+                                  <input type="text" name="adminPassword"><br>
+                                  <input type="submit" name="btnChange" 
+                                  value="Change"/>
                               </form>
                               <h1>CREATE NEW ADMIN ACCOUNT</h1>
                               <form action ="AdminControl.php" method="post">
-                                  <label>Enter Your Email: </label><input type="text" name="createEmail"><br>
-                                  <label>Choose the Password: </label><input type="text" name="createPassword"><br>
-                                  <p style="font-size=6px">Password must be 8 characters or longer and have <br> one lower case, one upper case, and one number</p>
-                                  <input type="submit" name="btnCreate" value="Create"/>
+                                  <label>Enter Your Email: </label>
+                                  <input type="text" name="createEmail"><br>
+                                  <label>Choose the Password: </label>
+                                  <input type="text" name="createPassword"><br>
+                                  <p style="font-size=6px">Password must be 8 
+                                  characters or longer and have
+                                   <br> one lower case, one upper case,
+                                    and one number</p>
+                                  <input type="submit" name="btnCreate"
+                                   value="Create"/>
                               </form>
                               <h1>REMOVE A USER</h1>
                               <form action="AdminControl.php" method="post">
-                                   <label>User Email to Remove: </label><input type="text" name="userEmail"><br>
-                                  <input type="submit" name="btnRemove" value="Remove"/>
-                             </form>';
-                    }                
+                                   <label>User Email to Remove: </label>
+                                   <input type="text" name="userEmail"><br>
+                                  <input type="submit" name="btnRemove"
+                                   value="Remove"/>
+                        </form>';
+                }                
                 ?>
             </div>
         </div>
