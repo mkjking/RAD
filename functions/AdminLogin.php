b8a58a54e70051c1f84dcead3d57ecbf98c90a73
mkjking <m128155@tafe.wa.edu.au>
2019-11-28 14:35:57 +0800

    Apply PEAR code standard to AdminLogin.php

--- functions/AdminLogin.php
+++ functions/AdminLogin.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-	<!-- 
+    <!-- 
         This php file is to handle management of subscribed users.
         To safely represent the content of this file please contain it in a DIV
 
@@ -10,38 +10,54 @@
 
 
     <form action="Admin.php" method="post">
-        <label>Email: </label><input type="text" name="adminEmail" placeholder=""><br>
-        <label>Password: </label><input type="text" name="password" placeholder=""><br>
+        <label>Email: </label><input type="text"
+         name="adminEmail" placeholder=""><br>
+        <label>Password: </label><input type="text"
+         name="password" placeholder=""><br>
         <input type="submit" name="btnLogin" value="Login" />
     </form>
 
-	<?php
+    <?php
+        /**
+        PHP version 7
 
+        @category SQL
+
+        @package RAD
+
+        @author Original Author <mitchel_king@icloud.com>
+
+        @license http://www.php.net/license PHP license 7
+
+        @link http:/pear.php.net
+         **/
         $loginStatus = false;
 
-		//Perform a check for button click 
-        if (isset($_POST['btnLogin'])){
+        //Perform a check for button click 
+    if (isset($_POST['btnLogin'])) {
 
-            //Retreive password from input
-            $passwordEntered = $_POST["password"];
-            $emailEntered = $_POST["adminEmail"];
+        //Retreive password from input
+        $passwordEntered = $_POST["password"];
+        $emailEntered = $_POST["adminEmail"];
 
-            $sql = "SELECT *
-                    FROM admin_tbl";
+        $sql = "SELECT *
+                FROM admin_tbl";
 
-            $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql);
 
-            if ($row = mysqli_fetch_assoc($result))
-            {
-                $adminPassword = $row['Password'];
-                $adminEmail = $row['Email'];
-            }
-            
-            if ($passwordEntered === $adminPassword && $adminEmail === $emailEntered){
-                $loginStatus=true;
-            }else{
-                echo "<p style=\"font-size: 20px\"><span style=\"color: Red\"> Details Incorrect</span></p>";
-            }
+        if ($row = mysqli_fetch_assoc($result)) {
+            $adminPassword = $row['Password'];
+            $adminEmail = $row['Email'];
         }
-	?>
+            
+        if ($passwordEntered === $adminPassword 
+            && $adminEmail === $emailEntered
+        ) {
+            $loginStatus=true;
+        } else {
+            echo "<p style=\"font-size: 20px\"><span style=\"
+            color: Red\"> Details Incorrect</span></p>";
+        }
+    }
+    ?>
 </html>
