e8e41b85dea10d6d53d0e244f9581c838bd8bd47
mkjking <m128155@tafe.wa.edu.au>
2019-12-05 21:01:42 +0800

    Add subscription status  to usertable

--- functions/Usertable.php
+++ functions/Usertable.php
@@ -15,12 +15,31 @@
             if(mysqli_num_rows($result) > 0) {
                 echo "<table border = '1' align = 'center'>";
                 echo "<th> &nbsp Name &nbsp</th>" 
-                . "<th> &nbsp Email &nbsp</th>";
+                . "<th> &nbsp Email &nbsp</th>"
+                . "<th>&nbsp Monthly</th>"
+                . "<th>&nbsp Blasts</th>";
 
                 //Display table data
                 while ($row = mysqli_fetch_array($result)) {
-                echo "<tr><td> &nbsp" . $row['name'] . "&nbsp </td>"
-                . "<td> &nbsp" . $row['email'] . "&nbsp </td>";
+                    $monthly = (int)$row['news'];
+                    $blast = (int)$row['blast'];
+
+                    if($monthly == 0) {
+                        $monthly = 'No';
+                    }
+                    else
+                        $monthly = 'Yes';
+
+                    if($blast == 0) {
+                        $blast = 'No';
+                    }
+                    else
+                        $blast = 'Yes';
+
+                    echo "<tr><td> &nbsp" . $row['name'] . "&nbsp </td>"
+                    . "<td> &nbsp" . $row['email'] . "&nbsp </td>"
+                    . "<td> &nbsp" . $monthly . "&nbsp </td>"
+                    . "<td> &nbsp" . $blast . "&nbsp </td>";
                 }
                 echo "</table>";
             }
